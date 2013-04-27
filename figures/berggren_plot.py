bec275fb7112d6d59f22db67670c4f7d55c15afd
Nils Wireklint <nils.wireklint@gmail.com>
2013-04-27 16:38:14 +0200

    fixed bergren_plot

--- figures/berggren_plot.py
+++ figures/berggren_plot.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Wedge
-import numpy as np
+import matplotlib
+import scipy as sp
+
+
+matplotlib.rc('xtick', color='white')
+matplotlib.rc('ytick', color='white')
 
 def dual_half_circle(center, radius, angle=0, ax=None,
                      **kwargs):
@@ -11,29 +16,63 @@ def dual_half_circle(center, radius, angle=0, ax=None,
     if ax is None:
         ax = plt.gca()
     theta1, theta2 = angle, angle + 180
-    w1 = Wedge(center, radius, theta1, theta2, fc='w', **kwargs)
+    w1 = Wedge(center, radius, theta1, theta2, fc='b', **kwargs)
     ax.add_artist(w1)
     return w1
-
-def plot_axis(ax=None):
-    if ax is None:
-        ax = plt.gca()
-    l1 = vlines(0.5, 0.1, 0.9, color='k', linestyles='solid')
-    ax.add_artist(l1)
-    return l1
     
 fig, ax = plt.subplots()
 dual_half_circle((0.5,0.5), radius=0.4, angle=0, ax=ax)
-plot_axis
 ax.axis('equal')
 
+fig.set_figheight(6)
+fig.set_figwidth(6)
 
-l = plt.axvline(x=0.5, ymin=0.05, ymax=0.95, linewidth=1, color='k')
-l = plt.axhline(y=.5, xmin=0.05, xmax=0.95, color='k')
+rect = fig.patch
+rect.set_facecolor('white')
+    
+plt.axes(frameon=False)
+
+
+
+
+#axes
+plt.plot([0.05, 0.95],[0.5, 0.5], color='k')
+plt.plot([0.5, 0.5],[0.05, 0.95], color='k')
+
+#triangle 4th
+plt.plot([0.5, 0.65],[0.5, 0.4], color='k', linewidth=2)
+plt.plot([0.65, 0.8],[0.4, 0.5], color='k', linewidth=2)
+
+#triangle 3rd
+plt.plot([0.35, 0.5],[0.6, 0.5], color='k', linewidth=2)
+plt.plot([0.2, 0.35],[0.5, 0.6], color='k', linewidth=2)
+
+#extra emphassis
+plt.plot([0.1, 0.2],[0.5, 0.5], color='k', linewidth=2)
+plt.plot([0.8, 0.9],[0.5, 0.5], color='k', linewidth=2)
+
 plt.xlim([0,1])
+plt.ylim([0,1])
 
-ax_list[1].autoscale_on(False)
 
+plt.plot([0.5, 0.5],[0.65, 0.58], "o", markersize=7,
+markeredgewidth=1,markeredgecolor='k',
+markerfacecolor='None'
+)
+plt.plot([0.65, 0.35],[0.45, 0.55], 'x')
 
+r = 0.4
+x1 = sp.linspace(0, 0.4, 200)
+x2 = sp.linspace(0.4, 0, 200)
+y1 =[]
+y2= []
+for i in xrange(len(x1)):
+    y1.append(0.5 + sp.sqrt(r**2 - x1[i]**2))
+    x1[i]+= 0.5
+    y2.append(0.5 + sp.sqrt(r**2 - ( r-x2[i])**2))
+    x2[i] +=0.1
+
+plt.plot(x1,y1, color='k', linewidth=2)
+plt.plot(x2,y2, color='k', linewidth=2)
 
 plt.show()
