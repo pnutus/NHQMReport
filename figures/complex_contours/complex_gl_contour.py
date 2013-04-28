49aa6c2e073f6a539ae8f9549b4a0c5328465c30
Pontus Granström <pnutus@gmail.com>
2013-04-29 01:42:11 +0200

    Sigh... matplotlib…

--- figures/complex_contours/complex_gl_contour.py
+++ figures/complex_contours/complex_gl_contour.py
@@ -1,50 +1,40 @@
-from nhqm.calculations import QM as calc
+from nhqm.bases.gen_contour import triangle_contour
+import matplotlib
 import matplotlib.pyplot as plt
 import scipy as sp
 
 order = 50
-peak_x = 0.7
-peak_y = 0.07
-k_max = 2.5
+peak_x = 0.5
+peak_y = 0.5
+k_max = 1.8
 
-
+matplotlib.rc('text', usetex=False )
+matplotlib.rc('font', family='serif')
+matplotlib.rcParams['text.latex.unicode']=True
 
 fig = plt.figure()
 
-rect = fig.patch
-rect.set_facecolor('white')
-    
-plt.axes(frameon=False)
+
 plt.axis('off')
+plt.axis('equal')
 
-def res_index(eigvecs):
-    maxes = map(max, abs(eigvecs.T))
-    return maxes.index(min(maxes))
-
-contour = calc.triangle_contour(peak_x, peak_y, k_max, order/3)
+contour = triangle_contour(peak_x, peak_y, k_max, order/3)
 
 points, _ = contour
 plt.plot(sp.real(points), sp.imag(points), 'o', markersize=4,
 markeredgewidth=1,markeredgecolor='k',
 markerfacecolor='None')
 
-plt.plot([-0.05, 2.95],[0, 0], color='k')
-plt.plot([0, 0],[-0.12, 0.04], color='k')
 
-plt.xlim([-.1,3])
-plt.ylim([-.13,.05])
+plt.xlim([-.1,2])
+plt.ylim([-.6,.3])
 
-plt.arrow( 2.85, 0, 0.05, 0, fc="k", ec="w",linewidth=1,
-head_width=0.0051, head_length=0.06 )
+plt.arrow( -0.05, 0, 2, 0, color="k", 
+            length_includes_head=True, head_width = 0.03)
+plt.arrow( 0, -0.6, 0, 1,  color="k", 
+            length_includes_head=True, head_width = 0.03)
 
-plt.arrow( 0, -0.01,0, 0.05, fc="k", ec="w",linewidth=1,
-head_width=0.05, head_length=0.005 )
+fig.text(0.16, 0.78, r'Im $k$', fontsize=15)
+fig.text(0.83, 0.6, r'Re $k$', fontsize=15)
 
-fig.text(0.16, 0.82, r'Im $k$', fontsize=15)
-
-
-fig.text(0.83, 0.69, r'Re $k$', fontsize=15)
-
-
-
-plt.show()
+plt.savefig('complex_contour.pdf', transparent=True, bbox_inches='tight', pad_inches=0)
