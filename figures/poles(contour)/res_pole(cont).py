e9856d78480b3ad706161a52218067470568573c
Nils Wireklint <nils.wireklint@gmail.com>
2013-05-15 17:49:47 +0200

    added (worthless) sieripinski plot

--- figures/poles(contour)/res_pole(cont).py
+++ figures/poles(contour)/res_pole(cont).py
@@ -35,33 +35,33 @@ l = 1
 j = 1.5
 args = (problem, l, j)
 k_max=5
-order = 36
+order = 60
 
 peak_x = 0.2
 peak_y = 0.2
 
-sierp_x = 0.1
-sierp_y = -0.04j
-sierpa = 0.99*sierp_x
+sierp_x = 0.17
+sierp_y = -0.1j
+sierpa = 0.5*sierp_x
 sierpb = 2*sierp_y
 sierpc = 0.99*sierp_x + 2*sierp_y
 sierpd = 0.51*sierp_x + sierp_y
 sierpe = 1.49*sierp_x + sierp_y
-sierpf = 2*sierp_x + 2*sierp_y
-sierpg = 1.01*sierp_x + 2*sierp_y
-sierph = 1.01*sierp_x
+sierpf = 1.01*sierp_x + 2*sierp_y
+sierpg = 2*sierp_x + 2*sierp_y
+sierph = 1.5*sierp_x
 
 
-sierpinski = gauss_contour([0, sierpa, sierpb, sierpc, sierpd, sierpe, sierpf, sierpg, sierph, k_max], order/9)
+sierpinski = gauss_contour([0, sierpa, sierpb,  sierpg, sierph, k_max], [  9,19,39,19,  200])
 
-contours = [triangle_contour(peak_x, peak_y, k_max, order/3), gauss_contour([0, 0 -peak_y*1j, 2*peak_x -peak_y*1j, 2*peak_x, k_max], order/4), sierpinski]
+contours = [triangle_contour(peak_x, peak_y, k_max, 1), gauss_contour([0, 0 -peak_y*1j, 2*peak_x -peak_y*1j, 2*peak_x, k_max], [1,1,1,1]), sierpinski]
 
 V0 = -47
-k_res = sp.empty((len(contours), order), 'complex')
+k_res = []
 
 for m, contour in enumerate(contours):
     problem.V0=V0
-
+    k_res.append(sp.empty(len(contour[0]), 'complex'))
     QNums = namedtuple('qnums', 'l j k')
     Q = QNums(l=1, j=1.5, k=len(contour[0]))
     H = mom.hamiltonian(contour, problem, Q)
@@ -70,22 +70,22 @@ for m, contour in enumerate(contours):
     
     for i in xrange(len(eigvals)):
         print m,i
-        k_res[m,i] = sp.sqrt(2*problem.mass*eigvals[i])
-        if sp.real(k_res[m,i])<10 ** -6:
-            k_res[m,i] = abs(k_res[m,i]) * 1j
-        if sp.imag(k_res[m,i]) > 0:
-            k_res[m,i] = sp.conj(k_res[m,i])   
+        k_res[m][i] = sp.sqrt(2*problem.mass*eigvals[i])
+        if sp.real(k_res[m][i])<10 ** -6:
+            k_res[m][i] = abs(k_res[m][i]) * 1j
+        if sp.imag(k_res[m][i]) > 0:
+            k_res[m][i] = sp.conj(k_res[m][i])   
     
 cont0, _ = contours[0] 
-cont1, _ = contours[1]  
-pole0r = sp.real(k_res[0])
-pole0i = sp.imag(k_res[0])
-pole1r = sp.real(k_res[1])
-pole1i = sp.imag(k_res[1]) 
-res_pole    = sp.array([pole0r, pole0i, pole1r, pole1i])
-contour_array     = sp.array([sp.real(cont0), sp.imag(cont0),sp.real(cont1), sp.imag(cont1)])
+cont1, _ = contours[1]
+cont2, _ = contours[2]  
+
+triangle   = sp.array([sp.real(cont0),sp.imag(cont0),sp.real(k_res[0]),sp.imag(k_res[0])])
+square     = sp.array([sp.real(cont1),sp.imag(cont1),sp.real(k_res[1]),sp.imag(k_res[1])])
+sierpinski = sp.array([sp.real(cont2),sp.imag(cont2),sp.real(k_res[2]),sp.imag(k_res[2])])
 
 script_dir = os.path.dirname(os.path.realpath(__file__)) + "/"
 
-sp.savetxt(script_dir + "poles.data", res_pole.T)
-sp.savetxt(script_dir + "contours.data", contour_array.T)
+#sp.savetxt(script_dir + "square.data", square.T)
+#sp.savetxt(script_dir + "triangle.data", triangle.T)
+sp.savetxt(script_dir + "sierpinski.data", sierpinski.T)
