ca724e8ac15da8bd803de134622640947eab6e1f
Pontus Granström <pnutus@gmail.com>
2013-05-21 09:01:13 +0200

    Fixed E(r0) figure and many small todos.

--- figures/E(r0)/E(r0).py
+++ figures/E(r0)/E(r0).py
@@ -16,26 +16,23 @@ from nhqm.plot_helpers import *
 problem = He5
 osc.integration_order = 30
 problem.V0 = -52
-a=0.5
+a=0.1
 b=10
-r0list=sp.linspace(a,b,500)
+r0list=sp.linspace(a,b,200)
 omegalist=[]
 for r in r0list:
-    omegalist.append(1/(problem.mass*r*r))
-basis_size=100
+    omegalist.append(1/(problem.mass*r**2))
+basis_size=50
 k_max=3
 
 contour = gauss_contour([0, k_max], basis_size)
 points, _ = contour
-QNums = namedtuple('qnums', 'l j J M E m')
-Q = QNums(l=1, j=1.5, J=0, M=0, 
-          m=[-1.5, -0.5, 0.5, 1.5],
-          E=range(basis_size))
+QNums = namedtuple('qnums', 'l j')
+Q = QNums(l=1, j=1.5)
 
 res_osc = []
 
-trunc_at = 5
-trunc_val = 5
+truncation = 5
 
 for i,omega in enumerate(omegalist):
     print (i+1),":",omega
@@ -44,20 +41,8 @@ for i,omega in enumerate(omegalist):
     eigvals, _ = energies(H)
     res_osc.append(eigvals[0:20])
 
-for ev in res_osc:
-    for e in ev:
-        if e > trunc_at:
-            e = trunc_val
-    
-print "done"
-
-# range = []
-# for om in omegalist:
-    # range.append(1/sp.sqrt(problem.mass*om))
-
-# plt.plot(r0list,res_osc)
-# plt.show()
+clipped_res = sp.clip(res_osc, -sp.inf, truncation)
 
 script_dir = os.path.dirname(os.path.realpath(__file__)) + "/"
-sp.savetxt(script_dir + "E(omega).data", 
-            sp.array([r0list]+zip(*res_osc)).T)
+sp.savetxt(script_dir + "E(r0).data", 
+            sp.array([r0list]+zip(*clipped_res)).T)
