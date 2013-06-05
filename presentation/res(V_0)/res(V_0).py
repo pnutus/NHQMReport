03c605eb441419f66110cb67eebb4a22aaabb5ba
Pontus Granström <pnutus@gmail.com>
2013-06-05 09:46:44 +0200

    tikz animations

---
+++ presentation/res(V_0)/res(V_0).py
@@ -0,0 +1,55 @@
+from __future__ import division
+import scipy as sp
+from nhqm.bases import mom_space as mom
+from nhqm.problems import He5
+from nhqm.QM_helpers import energies
+from nhqm.bases.contours import triangle_contour, naive_triangle_contour, gauss_contour
+from nhqm.plot_helpers import *
+from numpy.linalg import norm
+from collections import namedtuple
+
+
+problem = He5 
+order = 10*3
+V0max = 70
+V0min = 40
+problem.Vso = -7.04
+QNums = namedtuple('qnums', 'l j')
+Q = QNums(l=1, j=1.5)
+k_max = 2.5
+
+peak_x = 0.17
+peak_y = 0.17
+
+def res_index(eigvecs):
+    maxes = map(max, abs(eigvecs.T))
+    return maxes.index(min(maxes))
+
+contour = triangle_contour(peak_x, peak_y, k_max, order/3)
+points, weights = contour
+
+frames = 500
+
+V0s = sp.linspace(56, 30, frames)
+
+result = sp.empty((order,2*frames))
+
+
+for i, V0 in enumerate(V0s):
+    problem.V0 = -V0
+    points, weights = contour   
+    H = mom.hamiltonian(contour, problem, Q)
+    eigvals, eigvecs = energies(H)
+    ks = sp.sqrt(2*problem.mass*eigvals).T
+    idx = res_index(eigvecs)
+    pole = ks[idx]
+    if sp.real(pole) < 1e-4 and abs(sp.imag(pole)) > 0.02:
+        ks[idx] = 1j*abs(pole)
+    result[:,i*2    ] = sp.real(ks)
+    result[:,i*2 + 1] = sp.imag(ks)
+
+import os
+script_dir = os.path.dirname(os.path.realpath(__file__)) + "/"
+sp.savetxt(script_dir + "contour.data", 
+    sp.vstack([sp.real(points), sp.imag(points)]).T)
+sp.savetxt(script_dir + "solutions.data", result)
---
+++ presentation/res(contour)/res(contour).py
@@ -0,0 +1,49 @@
+from __future__ import division
+import scipy as sp
+from nhqm.bases import mom_space as mom
+from nhqm.problems import He5
+from nhqm.QM_helpers import energies
+from nhqm.bases.contours import triangle_contour, naive_triangle_contour, gauss_contour
+from nhqm.plot_helpers import *
+from numpy.linalg import norm
+from collections import namedtuple
+
+
+fig = plt.figure(1)
+fig.clf()
+ax = fig.add_subplot(111)
+ax.axis([-0.02, 0.7, -0.11, 0.01])
+
+
+problem = He5 
+order = 10*3
+problem.V0 = -47.05
+problem.Vso = -7.04
+QNums = namedtuple('qnums', 'l j J M E m')
+Q = QNums(l=1, j=1.5, J=0, M=0, 
+          m=[-1.5, -0.5, 0.5, 1.5], 
+          E=range(order))
+k_max = 2.5
+
+peak_x = 0.17
+
+frames = 100
+
+peak_ys = sp.linspace(0, 0.1, frames)
+
+result = sp.empty((order,4*frames))
+
+for i, peak_y in enumerate(peak_ys):
+    contour = triangle_contour(peak_x, peak_y, k_max, order/3)
+    points, weights = contour
+    H = mom.hamiltonian(contour, problem, Q)
+    eigvals, eigvecs = energies(H)
+    ks = sp.sqrt(2*problem.mass*eigvals)
+    result[:,i*4    ] = sp.real(points).T
+    result[:,i*4 + 1] = sp.imag(points).T
+    result[:,i*4 + 2] = sp.real(ks).T
+    result[:,i*4 + 3] = sp.imag(ks).T
+
+import os
+script_dir = os.path.dirname(os.path.realpath(__file__)) + "/"
+sp.savetxt(script_dir + "res(contour).data", result)
